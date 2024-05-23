12cdf1c757467365b22feed51d2ff117f9edad9b
quan2.duong <quan2.duong@lge.com>
2024-05-23 09:17:39 +0700

    update

--- dp/1937_B.cpp
+++ dp/1937_B.cpp
@@ -23,10 +23,10 @@ int32_t main() {
         for (int i=0; i<n; i++) cin >> grid[0][i];
         for (int i=0; i<n; i++) cin >> grid[1][i];
 
-        int cut = -1;
-        for (int i=1; i<n; i++) {
-            if ((grid[0][i] - '0') > (grid[1][i-1] - '0')) {
-                cut = i - 1;
+        int cut = n-1;
+        for (int i=0; i<n-1; i++) {
+            if ((grid[0][i+1] - '0') > (grid[1][i] - '0')) {
+                cut = i;
                 break;
             }
         }
@@ -40,11 +40,13 @@ int32_t main() {
         dp[0][0] = 1;
         for (int i=1; i<n; i++) {
             if (grid[0][i] == ans[i]) dp[0][i] = 1;
-            // else break;
+            else break;
         }
 
-        for (int i=0; i<n; i++) {
-            if (grid[1][i] == ans[i+1]) dp[1][i] = dp[0][i] + (i > 0 ? dp[1][i-1] : 0);
+        dp[1][0] = (grid[0][1] >= grid[1][0] ? 1 : 0);
+
+        for (int i=1; i<n; i++) {
+            if (grid[1][i] == ans[i+1]) dp[1][i] = dp[0][i] + dp[1][i-1];
         }
 
         // cout << "start\n";
