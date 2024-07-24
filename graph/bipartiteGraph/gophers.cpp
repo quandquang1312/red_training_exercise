f3b5fa99ddf46f81e4b22bf55e4ce2efe0f0aaa2
quandquang1312 <quangquanb96@gmail.com>
2024-07-24 23:06:44 +0700

    f

--- graph/bipartiteGraph/gophers.cpp
+++ graph/bipartiteGraph/gophers.cpp
@@ -1,64 +1,72 @@
-// https://open.kattis.com/problems/gopher2?tab=metadata
+#include <iostream>
+#include <complex>
+#include <vector>
 
-#include <bits/stdc++.h>
 using namespace std;
 
 vector<bool> visited;
-vector<int> LeftMatch, RightMatch;
-vector<vector<int>> adj;
 
-bool kuhn(int u) {
+bool kuhn(int u, const vector<vector<int>> &adj, vector<int> &RightMatch, vector<int> &LeftMatch) {
     if (visited[u]) return false;
     visited[u] = true;
-
-    for (int v : adj[u]) {
-        if (LeftMatch[v] == -1 || kuhn(LeftMatch[v])) {
-            LeftMatch[v]  = u;
+    for (auto &v : adj[u]) {
+        if (LeftMatch[v] == -1 || (kuhn(LeftMatch[v], adj, RightMatch, LeftMatch))) {
+            LeftMatch[v] = u;
             RightMatch[u] = v;
             return true;
         }
     }
-
     return false;
 }
 
-int32_t main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
+void solve(int N, int M, const vector<vector<int>> &adj)
+{
+    vector<int> RightMatch(N, -1), LeftMatch(M, -1);
+    for (int i = 0; i < N; i++)
+    {
+        if (RightMatch[i] != -1) continue;
+        visited.assign(200, false);
+        kuhn(i, adj, RightMatch, LeftMatch);
+    }
+
+    int ans = 0;
+    for (int i = 0; i < N; i++) if (RightMatch[i] == -1) ans++;
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    int N, M, S, V;
+    double x, y;
 
     #ifdef LOCAL
         freopen("../in.txt", "r", stdin);
         freopen("../ou.txt", "w", stdout);
     #endif
 
-    int n, m, s, v;
-    while (cin >> n >> m >> s >> v) {
-        adj.clear();
-        adj.assign(n, vector<int>());
-        vector<complex<double>> gops(n), holes(m); 
+    while (cin >> N >> M >> S >> V)
+    {
+        vector<complex<double>> gophers(N), holes(M);
+        for (int i = 0; i < N; i++)
+        {
+            cin >> x >> y;
+            gophers[i] = {x, y};
+        }
 
-        for (int i=0; i<n; i++) { double x, y; cin >> x >> y; gops[i] = {x, y}; };
-        for (int i=0; i<m; i++) { double x, y; cin >> x >> y; holes[i] = {x, y}; };
+        for (int i = 0; i < M; i++)
+        {
+            cin >> x >> y;
+            holes[i] = {x, y};
+        }
 
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (abs(gops[i] - holes[j]) <= (s * v)) adj[i].push_back(j);
+        vector<vector<int>> adj(N);
+        for (int u = 0; u < N; u++) {
+            for (int v = 0; v < M; v++) {
+                if (abs(gophers[u] - holes[v]) <= S * V) adj[u].push_back(v);
             }
         }
 
-        LeftMatch.resize(n+m, -1);
-        RightMatch.resize(n+m, -1);
-
-        for (int i=0; i<n; i++) {
-            if (RightMatch[i] != -1) continue;
-            visited.resize(n+m, false);
-            kuhn(i);
-        }
-
-        int ans = 0;
-        for (int i=0; i<n; i++) if (RightMatch[i] == -1) ans++;
-        cout << ans << endl;
+        solve(N, M, adj);
     }
-
-    return 0;
 }
