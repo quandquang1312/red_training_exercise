1bb9bea980d60ca7ab9c2c49bcb3bed1bd2ca9e8
quandquang1312 <quangquanb96@gmail.com>
2024-08-05 23:24:31 +0700

    f

--- tree/cj25123.cpp
+++ tree/cj25123.cpp
@@ -4,41 +4,55 @@
 using namespace std;
 
 #define int long long
-vector<int> dp;
-
-int dfs(int u, vector<vector<int>> adj, string tr, int r) {
-    if (dp[u] != 0) return dp[u];
-
-    int ans = 0;
+vector<pair<int, int>> dp1, dp2;
+pair<int, int> dfs_1(int u, int par, vector<vector<int>>& adj, string tr) {
+    if (dp1[u].first != -1) return dp1[u];
+    int ans = 0, cnt = 1;
     for (auto &v : adj[u]) {
-        if (v == r || tr[v-1] == tr[u-1]) continue;
-        ans = max(ans, dfs(v, adj, tr, u));
+        if (v == par || tr[v-1] == tr[u-1]) continue;
+        pair<int, int> tmp_ans = dfs_1(v, u, adj, tr);
+        if (tmp_ans.first == ans) cnt += tmp_ans.second;
+        else if (tmp_ans.first > ans) ans = tmp_ans.first, cnt = tmp_ans.second;
     }
 
-    return dp[u] = ans + 1;
+    return dp1[u] = {1 + ans, cnt};
+}
+
+pair<int, int> dfs_2(int u, int par, vector<vector<int>>& adj, string tr) {
+    if (dp2[u].first != -1) return dp2[u];
+    int ans = 0, cnt = 1;
+    for (auto &v : adj[u]) {
+        if (v == par || tr[v-1] == tr[u-1] || v < u) continue;
+        pair<int, int> tmp_ans = dfs_2(v, u, adj, tr);
+        if (tmp_ans.first == ans) cnt += tmp_ans.second;
+        else if (tmp_ans.first > ans) ans = tmp_ans.first, cnt = tmp_ans.second;
+    }
+
+    return dp2[u] = {1 + ans, cnt};
 }
 
 void sub_1(int n, string tr, vector<int> par) {
-    dp.assign(n+1, 0);
     vector<vector<int>> adj(n+1);
-    for (int i=0; i<n; i++) {
+    for (int i=1; i<=n; i++) {
         if (par[i] == 0) continue;
-        adj[i+1].push_back(par[i]);
-        adj[par[i]].push_back(i+1);
+        adj[i].push_back(par[i]);
+        adj[par[i]].push_back(i);
+    }
+
+    int cnt = 0, ans = 0;
+    for (int i=1; i<=n; i++) {
+        pair<int, int> tmp_ans = dfs_1(i, 0, adj, tr);
+        if (tmp_ans.first == ans) cnt += tmp_ans.second;
+        else if (tmp_ans.first > ans) ans = tmp_ans.first, cnt = tmp_ans.second;
     }
 
     for (int i=1; i<=n; i++) {
-        cout << i << ": ";
-        for (auto &v : adj[i]) {
-            cout << v << " ";
-        } cout << endl;
+        pair<int, int> tmp_ans = dfs_2(i, 0, adj, tr);
+        if (tmp_ans.first == ans) cnt += tmp_ans.second;
+        else if (tmp_ans.first > ans) ans = tmp_ans.first, cnt = tmp_ans.second;
     }
 
-    for (int i=1; i<=n; i++) if (dp[i] == 0) dfs(i, adj, tr, 0);
-    cout << "====\n";
-    for (int i=1; i<=n; i++) {
-        cout << i << ": " << dp[i] << '\n';
-    }
+    cout << ans << " " << (ans != 1 ? cnt/2 : cnt) << '\n';
 }
 
 void sub_2(int n, string tr, vector<int> par) {
@@ -58,10 +72,12 @@ int32_t main() {
     while (tc--) {
         int n; cin >> n;
         string tr; cin >> tr;
-        vector<int> par(n);
-        for (int i=0; i<n; i++) cin >> par[i];
+        dp1.assign(n+1, {-1, -1});
+        dp2.assign(n+1, {-1, -1});
+        vector<int> par(n + 1);
+        for (int i=1; i<=n; i++) cin >> par[i];
         if (n <= 1000) sub_1(n, tr, par);
-        else sub_2(n, tr, par);
+        else sub_1(n, tr, par);
     }
 
     return 0;
