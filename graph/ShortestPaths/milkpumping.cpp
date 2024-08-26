524d918c1756dfb57308415874b3152d9bc94dbd
quan2.duong <quan2.duong@lge.com>
2024-08-26 17:53:40 +0700

    af

--- graph/ShortestPaths/milkpumping.cpp
+++ graph/ShortestPaths/milkpumping.cpp
@@ -6,6 +6,32 @@ using namespace std;
 #define int long long
 #define INF LLONG_MAX
 
+int dijkstra(int minf, int n, vector<vector<tuple<int, int, int>>>& adj) {
+    priority_queue<pair<int, int>> pq;
+    pq.push({0, 1});
+    vector<int> dist(n + 1, INF);
+    dist[1] = 0;
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+
+        d = -d;
+
+        if (d > dist[u]) continue;
+
+        for (auto &[v, w, f] : adj[u]) {
+            if (f < minf) continue;
+            if (d + w < dist[v]) {
+                dist[v] = d + w;
+                pq.push({-dist[v], v});
+            }
+        }
+    }
+
+    return dist[n];
+}
+
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
@@ -22,35 +48,24 @@ int32_t main() {
     cin >> n >> m;
 
     vector<vector<tuple<int, int, int>>> adj(n + 1);
+
+    vector<int> flist;
     for (int i=0, u, v, w, f; i<m; i++) {
         cin >> u >> v >> w >> f;
         adj[u].push_back({v, w, f});
         adj[v].push_back({u, w, f});
+        flist.push_back(f);
     }
 
-    priority_queue<tuple<int, int, int>> pq;
-    pq.push({0, 1, INF});
-    vector<pair<int, int>> dist(n + 1, {INF, INF});
-    dist[1] = {0, INF};
-
-    while (!pq.empty()) {
-        auto [d, u, cf] = pq.top();
-        pq.pop();
-
-        d = -d;
-
-        if (d > dist[u].first) continue;
-
-        for (auto &[v, w, f] : adj[u]) {
-            if (d + w < dist[v].first) {
-                dist[v] = {d + w, min(cf, f)};
-                pq.push({-dist[v].first, v, min(cf, f)});
-            }
-        }
+    int ans = -1;
+    for (int i=0; i<flist.size(); i++) {
+        int tmp_dst = dijkstra(flist[i], n, adj);
+        if (tmp_dst == INF) continue;
+        double ratio = (double) flist[i] / (double) tmp_dst;
+        ans = max(ans, (int)(ratio * 1e6));
     }
 
-    int ans = ((1.0 * dist[n].second) / (1.0 * dist[n].first)) * 1e6;
-    cout << ans << '\n';
+    cout << ans << endl;
 
     return 0;
 }
