367b36839ecd3e759b5f5a44a048168824cc84fa
quan2.duong <quan2.duong@lge.com>
2024-10-03 17:00:38 +0700

    Daily Practice

--- speedrun/tree_contest/E.cpp
+++ speedrun/tree_contest/E.cpp
@@ -6,13 +6,17 @@ using namespace std;
 #define int long long
 
 vector<vector<int>> adj;
-vector<int> startIdx, endIdx;
+vector<int> startIdx, endIdx, parent;;
+vector<int> val;
 int timer = 1;
 
 void euler_tour(int at, int prev) {
     startIdx[at] = timer++;
     for (int n : adj[at]) {
-        if (n != prev) { euler_tour(n, at); }
+        if (n != prev) { 
+            parent[n] = at;
+            euler_tour(n, at);
+        }
     }
     endIdx[at] = timer;
 }
@@ -20,13 +24,13 @@ void euler_tour(int at, int prev) {
 class SegmentTree {
 private:
     int n;
-    vector<int> tree;
+    vector<double> tree;
 
-    int merge(int a, int b) {
+    double merge(double a, double b) {
         return a * b;
     }
 
-    void update(int node, int tl, int tr, int pos, int val) {
+    void update(int node, int tl, int tr, int pos, double val) {
         if (tl == tr) {
             tree[node] = val;
         } else {
@@ -39,8 +43,8 @@ private:
         }
     }
 
-    int query(int node, int tl, int tr, int l, int r) {
-        if (l > r) return 1;
+    double query(int node, int tl, int tr, int l, int r) {
+        if (l > r) return 1.0;
         if (l == tl && r == tr) {
             return tree[node];
         }
@@ -52,19 +56,18 @@ private:
 public:
     SegmentTree(int sz) : n(sz) {
         // Assign 1
-        tree.assign(4 * n + 5, 1);
+        tree.assign(4 * n + 5, 1.0);
     }
 
-    void update(int pos, int val) {
+    void update(int pos, double val) {
         update(1, 1, n, pos, val);
     }
 
-    int query(int l, int r) {
+    double query(int l, int r) {
         return query(1, 1, n, l, r);
     }
 };
 
-vector<int> val;
 
 int brute(int u, int prev) {
     int ans = val[u];
@@ -90,19 +93,29 @@ int32_t main() {
     startIdx.assign(n * 2, 0);
     endIdx.assign(n * 2, 0);
     val.assign(n, 1);
+    parent.assign(n, 0);
     adj.resize(n);
 
     for (int i=0, u, v; i<n-1; i++) {
         cin >> u >> v;
         u--, v--;
-
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
     euler_tour(0, 0);
 
-    SegmentTree st(n * 2 + 5);
+    // for (int i=0; i<n; i++) {
+    //     cout << i << ": " << parent[i] << " - " << endIdx[i] << "\n"; 
+    // }
+
+    SegmentTree st(timer + 5);
+    auto prt = [&]() {
+        for (int i=1; i<timer; i++) {
+            cout << st.query(i,i) << " ";
+        }
+        cout << "\n";
+    };
 
     int q; cin >> q;
     while (q--) {
@@ -110,18 +123,25 @@ int32_t main() {
         if (t == 1) {
             x--;
             st.update(startIdx[x], y);
-            st.update(endIdx[x], -y);
-            val[x] = y;
+            // double update_y = (double) (1.0 / y);
+            // st.update(endIdx[x], update_y);
+            // if (x) st.update(endIdx[parent[x]], y);
+            // // cout << "update: " << x << " - " << y << "\n";
+            // val[x] = y;
         } else { // t == 2
             x--, y--;
-            int product_x = brute(x, x);
-            int product_y = brute(y, y);
-            // int product_x = st.query(startIdx[x], endIdx[x]);
-            // int product_y = st.query(startIdx[y], endIdx[y]);
+            // int product_x_brute = brute(x, parent[x]);
+            // int product_y_brute = brute(y, parent[y]);
+            double product_x = st.query(startIdx[x], endIdx[x] - 1);
+            double product_y = st.query(startIdx[y], endIdx[y] - 1);
+            // cout << product_x << " - " << product_y << "\n";
             double ans = (1.0 * product_x) / (1.0 * product_y);
-            if (ans > 1000000000) printf("1000000000\n");
-            else printf("%.6f\n", ans);
+            // double ans_brute = (1.0 * product_x_brute) / (1.0 * product_y_brute);
+            // cout << ans << " <-> " << " " << "\n";
+            if (ans > 1000000000) cout << "1000000000\n";
+            else cout << ans << "\n";
         }
+        // prt();
     }
 
     return 0;
