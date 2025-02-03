6fa934644abb5a0b62c9be8586e18eaf771517e5
quan2.duong <quan2.duong@lge.com>
2025-02-03 18:23:18 +0700

    Daily Practice

---
+++ dp/bitmask/2051_G.cpp
@@ -0,0 +1,47 @@
+// https://codeforces.com/contest/2051/problem/G
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n, q; cin >> n >> q;
+
+    vector<vector<int>> dist(n + 1, vector<int>(n + 1, 0));
+
+    vector<pair<int, char>> queries(q);
+    for (int i=0, x; i<q; i++) {
+        char c; cin >> x >> c;
+        queries[i] = {x, c};
+    }
+
+    for (int x=1; x<=n; x++) {
+        for (int y=1; y<=n; y++) {
+            if (x == y) continue;
+            string str;
+            for (int i=0; i<q; i++) {
+                if (queries[i].first == x && queries[i].second == '+')
+                    str.push_back('+');
+                if (queries[i].first == y && queries[i].second == '-')
+                    str.push_back('-');
+            }
+
+            int cur_m = 0;
+            for (int j=0; j<str.size(); j++) {
+                if (str[j] == '-') cur_m++;
+                else {
+                    if (cur_m) cur_m--;
+                    else dist[x][y]++;
+                }
+            }
+
+            cout << x << "->" << y << ": " << dist[x][y] << "\n";
+        }
+    }
+
+    return 0;
+}
