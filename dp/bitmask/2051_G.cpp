cd79733645ec6e78fa68610bb17a1d3e21a69b79
quan2.duong <quan2.duong@lge.com>
2025-02-04 10:20:47 +0700

    Daily Practice

--- dp/bitmask/2051_G.cpp
+++ dp/bitmask/2051_G.cpp
@@ -5,24 +5,57 @@ using namespace std;
 
 #define int long long
 
+const int INF = 1e18;
+
+vector<vector<int>> dp, dist;
+vector<int> last_length;
+int n;
+
+int tsp() {
+    int ans = INF;
+
+    for (int i=0; i<n; i++)
+        dp[1 << i][i] = 0;
+
+    for (int mask=1; mask<(1 << n); mask++) {
+        for (int i=0; i<n; i++) {
+            if (!(mask & (1 << i))) continue;
+            for (int j=0; j<n; j++) {
+                if (i == j) continue;
+                if (!(mask & (1 << j))) continue;
+                int prev_mask = mask ^ (1 << i);
+                dp[mask][i] = min(dp[mask][i], dist[j][i] + dp[prev_mask][j]);
+            }
+        }
+    }
+
+    for (int i=0; i<n; i++)
+        ans = min(ans, dp[(1 << n) - 1][i] + last_length[i]);
+
+    return ans;
+}
+
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int n, q; cin >> n >> q;
+    int q; cin >> n >> q;
 
-    vector<vector<int>> dist(n + 1, vector<int>(n + 1, 0));
+    dp.assign(1 << n, vector<int>(n, INF));
+    dist.assign(n, vector<int>(n, 0));
+    last_length.assign(n, 0);
 
     vector<pair<int, char>> queries(q);
     for (int i=0, x; i<q; i++) {
         char c; cin >> x >> c;
+        x--;
         queries[i] = {x, c};
+        if (c == '+') last_length[x]++;
     }
 
-    for (int x=1; x<=n; x++) {
-        for (int y=1; y<=n; y++) {
+    for (int x=0; x<n; x++) {
+        for (int y=0; y<n; y++) {
             if (x == y) continue;
-            string str;
             int cur_m = 0;
             for (int i=0; i<q; i++) {
                 if (queries[i].first == x && queries[i].second == '+') {
@@ -34,10 +67,12 @@ int32_t main() {
                     cur_m++;
                 }
             }
-
-            cout << x << "->" << y << ": " << dist[x][y] << "\n";
         }
     }
+
+    int ans = tsp();
+
+    cout << n + ans << "\n";
 
     return 0;
 }
